b57f0fbcf92ff276705242fe1b126871c3c49973
nvijay9210 <vijaybeeee2020@gmail.com>
2025-07-14 11:17:45 +0530

    redis_config

--- utils/RedisCache.js
+++ utils/RedisCache.js
@@ -1,12 +1,12 @@
-const redis = require('../config/redisConfig'); // Import the Redis client
+const redis = require("../config/redisConfig"); // Import the Redis client
 
 // Set cache with TTL (Time-To-Live)
 const setCache = async (key, value, ttl = 3600) => {
   try {
-    await redis.set(key, JSON.stringify(value), 'EX', ttl); // 'EX' sets the TTL
+    await redis.set(key, JSON.stringify(value), "EX", ttl); // 'EX' sets the TTL
     console.log(`Data cached for key: ${key}`);
   } catch (err) {
-    console.error('Error setting cache:', err);
+    console.error("Error setting cache:", err);
   }
 };
 
@@ -18,18 +18,17 @@ const getCache = async (key, fallbackFunction) => {
       console.log(`Serving data for key: ${key} from cache`);
       return JSON.parse(cachedData);
     }
-    
+
     // If cache misses, fetch fresh data and cache it
     console.log(`Cache miss for key: ${key}, fetching fresh data`);
     const freshData = await fallbackFunction();
     await setCache(key, freshData);
     return freshData;
   } catch (err) {
-    console.error('Error fetching cache:', err);
+    console.error("Error fetching cache:", err);
     throw err; // Propagate error for handling elsewhere
   }
 };
-
 
 function buildCacheKey(type, scope, options = {}) {
   const parts = [type, scope];
@@ -51,7 +50,7 @@ function buildCacheKey(type, scope, options = {}) {
     limit,
     start_date,
     end_date,
-    appointment_type
+    appointment_type,
     // Add more fields as needed
   } = options;
 
@@ -63,9 +62,12 @@ function buildCacheKey(type, scope, options = {}) {
   if (asset_id) parts.push(`asset_id:${asset_id}`);
   if (expense_id) parts.push(`expense_id:${expense_id}`);
   if (supplier_id) parts.push(`supplier_id:${supplier_id}`);
-  if (supplier_product_id) parts.push(`supplier_product_id:${supplier_product_id}`);
-  if (supplier_payment_id) parts.push(`supplier_payment_id:${supplier_payment_id}`);
-  if (supplier_review_id) parts.push(`supplier_review_id:${supplier_review_id}`);
+  if (supplier_product_id)
+    parts.push(`supplier_product_id:${supplier_product_id}`);
+  if (supplier_payment_id)
+    parts.push(`supplier_payment_id:${supplier_payment_id}`);
+  if (supplier_review_id)
+    parts.push(`supplier_review_id:${supplier_review_id}`);
   if (purchase_id) parts.push(`purchase_id:${purchase_id}`);
 
   if (page !== undefined) parts.push(`page:${page}`);
@@ -74,7 +76,7 @@ function buildCacheKey(type, scope, options = {}) {
   if (end_date) parts.push(`end_date:${end_date}`);
   if (appointment_type) parts.push(`appointment_type:${appointment_type}`);
 
-  return parts.join(':');
+  return parts.join(":");
 }
 
-module.exports={getCache,buildCacheKey}
+module.exports = { getCache, buildCacheKey };
