4596bc1b68768a86ffc1f9d5212ba27ddd48a787
nvijay9210 <vijaybeeee2020@gmail.com>
2025-08-09 13:16:28 +0530

    fileurl

--- services/SupplierProductsService.js
+++ services/SupplierProductsService.js
@@ -183,7 +183,7 @@ const getAllSupplierProductssByTenantIdAndSupplierId = async (
         // Extract only file_url
         const fileInfos = docs.map((doc) => ({
           document_id: doc.document_id,
-          image_url: doc.file_url,
+          file_url: doc.file_url,
         }));
 
         return {
